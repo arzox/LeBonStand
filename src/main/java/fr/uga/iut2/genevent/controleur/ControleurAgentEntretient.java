8b15da066a9b846abe586b267398b568d8403d41
ratgrism <morgan.ratgris@etu.univ-grenoble-alpes.fr>
2024-06-14 15:09:59 +0200

    controlleur : commercant - participant

--- src/main/java/fr/uga/iut2/genevent/controleur/ControleurAgentEntretient.java
+++ src/main/java/fr/uga/iut2/genevent/controleur/ControleurAgentEntretient.java
@@ -1,12 +1,19 @@
 package fr.uga.iut2.genevent.controleur;
 
 import fr.uga.iut2.genevent.modele.Application;
+import fr.uga.iut2.genevent.modele.Evenement;
 
 public class ControleurAgentEntretient {
 
     private Application application;
+    private Evenement evenement;
 
     public ControleurAgentEntretient(Application application) {
         this.application = application;
     }
+
+    public void setEvenement(Evenement evenement) {
+        this.evenement = evenement;
+    }
+
 }
